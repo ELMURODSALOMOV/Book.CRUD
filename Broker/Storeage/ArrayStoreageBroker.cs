daf684f74ebbc535a10cc213ad801ab2dc70b9d3
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-03-20 16:03:30 +0500

    Read Book

---
+++ Broker/Storeage/ArrayStoreageBroker.cs
@@ -0,0 +1,37 @@
+﻿using Book.CRUD.Models;
+using System.Data.Common;
+
+namespace Book.CRUD.Broker.Storeage
+{
+    internal class ArrayStoreageBroker : IStoreageBroker
+    {
+        private Books[] BooksInfo { get; set; } = new Books[10];
+        public ArrayStoreageBroker()
+        {
+            BooksInfo[0] = new Books()
+            {
+                Id = 1,
+                Name = "O'tgan kunlar",
+                Author = "Abdulla Qodiriy"
+            };
+            BooksInfo[1] = new Books()
+            {
+                Id = 2,
+                Name = "Shum bola",
+                Author = "G'ofur G'ulom"
+            };
+        }
+        public Books ReadBook(int id)
+        {
+            for(int itaration = 0; itaration < BooksInfo.Length; itaration++)
+            {
+                Books bookInfoLine = BooksInfo[itaration];
+                if (bookInfoLine.Id == id)
+                {
+                    return bookInfoLine;
+                }
+            }
+            return new Books();
+        }
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace Book.CRUD.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
